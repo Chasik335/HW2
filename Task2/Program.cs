9266054baa883991becf089c7f820bb802f5ff1b
Vladislav <vhasovnikov@inbox.ru>
2023-01-08 17:35:02 +0500

    Нарисовал блок схему, написал код, добавил кнопки в readme (по первой второй задачи)

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100)
+{
+    int endCh = number % 10;
+    Console.WriteLine(endCh);
+}
+else
+{
+   Console.Write("Третьего числа нет  -1"); 
+}
+
