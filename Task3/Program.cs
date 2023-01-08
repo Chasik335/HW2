f1acc033ea48c4ed19e6a3ca5c9ea3dbbff33c4c
Vladislav <vhasovnikov@inbox.ru>
2023-01-08 18:28:04 +0500

    Нарисовал блок схему, написал код, добавил кнопки в readme (по третьей задачи)

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (1 <= number && number <= 7)
+{
+    if (number > 5)
+    {
+        Console.Write("Выходной день");
+    }
+    else
+    {
+        Console.Write("Рабочий день");
+    }
+}
+else
+{
+    Console.Write("К сожалению в календаре нет заданного дня");
+}
